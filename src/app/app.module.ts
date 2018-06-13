f9686506924bd8cfcfb7935ebce8216c88dc74ed
Jonas Arguelles <jonasa@sourcepad.com>
2018-06-13 21:56:51 +0800

    working on forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule, Http } from '@angular/http';
 import { AppComponent } from './app.component';
 
@@ -50,6 +50,7 @@ const PROVIDERS = [
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     SocialLoginModule,
     RouterModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { AuthService } from "angularx-social-login";
 import { SocialUser } from "angularx-social-login";
 import { GoogleLoginProvider } from "angularx-social-login";
-import { SessionService } from '../services/api/session';
+import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+
+import { SessionService } from '../services/api/session';
+import { EmailValidator } from '../validators';
 import { environment } from '../../environments/environment'
 import { LocalStorage } from '../services/util'
 
@@ -21,14 +24,39 @@ declare const gapi: any;
 export class LoginComponent implements OnInit, AfterViewInit {
 
   user: SocialUser;
+  showForm = false;
+  form: FormGroup;
   // private loggedIn: boolean;
 
   constructor(
     private authService: AuthService,
     private session: SessionService,
     private activeRoute: ActivatedRoute,
-    private localStorage: LocalStorage
-  ) { }
+    private localStorage: LocalStorage,
+    fb: FormBuilder
+  ) {
+    this.form = fb.group({
+      // 'display_name': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'name': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'department': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'position': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'mobile_number': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'skype': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'office_location': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'start_date': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'age': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'tin_number': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'sss_number': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'address': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'person_to_notify': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'person_to_notify_mobile': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'is_active': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      'email': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'us_phone': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      // 'birthday': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+    });
+   }
 
   ngOnInit() {
   }
@@ -83,11 +111,11 @@ export class LoginComponent implements OnInit, AfterViewInit {
           console.log("login is updated")
           this.session.setSession(res.user);
           this.redirectUser(routeParams, res.user)
-          // if (res.user.is_verified) {
-          //   this.redirectUser(routeParams, res.user)
-          // } else {
-          //   console.log("show form")
-          // }
+          if (res.user.is_verified) {
+            this.redirectUser(routeParams, res.user)
+          } else {
+            console.log("show form")
+          }
        });
       }
     }
