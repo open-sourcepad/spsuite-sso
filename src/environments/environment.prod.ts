51fe96b66272b2e8a2fb096890cdae3d1b8ffd0c
Adrian Agbayani <adrianda@sourcepad.com>
2018-06-29 15:18:20 +0800

    Fixed Urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  base_url: 'https://sso.spsuite.com/',
-  api_url: 'https://api.spsuite.com/',
+  base_url: 'https://sso.spsuite.co/',
+  api_url: 'https://api.spsuite.co/',
   google_client: '42075831752-9jf638265r2pirv7m31h141dnpf4hkas.apps.googleusercontent.com'
 };
